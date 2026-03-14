b0351ea5c48f84d959e189a6c0eb11a3565a261d
Rana-devanand <gdsc.devanandrana@gmail.com>
2026-03-14 16:52:21 +0530

    chore: Configure CORS to explicitly allow all origins.

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ const app: Express = express();
 const port = Number(process.env.PORT) || 5000;
 
 // ── Standard Middlewares
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(morgan("dev"));
